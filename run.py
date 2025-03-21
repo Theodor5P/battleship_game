ef2c9ba3b626eb21ede0d58706a5fbb765f6c942
Theodor5P <wwtlx9hw@students.codeinstitute.net>
2025-03-21 08:09:56 +0100

    create class Ship

--- run.py
+++ run.py
@@ -3,3 +3,20 @@ import os
 import time
 
 
+class Ship:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+        self.hits = 0
+        self.coordinates = []
+        self.is_sunk = False
+
+    def hit(self):
+        self.hits += 1
+        if self.hits == self.size:
+            self.is_sunk = True
+            return True
+        return False
+    
+
+    
